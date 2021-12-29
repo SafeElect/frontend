12927aa1ed7f1fcbf5869be923f5529e6fc94618
Syed Abdullah Hassan <abdullah.hassan@ug.bilkent.edu.tr>
2021-12-30 02:38:37 +0300

    setup
    next.js + tailwind

---
+++ pages/register.js
@@ -0,0 +1,5 @@
+function register() {
+    return <h1 >Register page</h1>
+  }
+  
+  export default register
---
+++ pages/vote.js
@@ -0,0 +1,5 @@
+function vote() {
+    return <h1 className="text-3xl font-bold">Vote page</h1>
+  }
+  
+  export default vote
